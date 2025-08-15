980f149ce8877d50eea790d960591c544fe88c21
Tanmay <tanmaykpatel03@gmail.com>
2025-08-15 04:05:18 +0000

    logger update

--- src/logger.py
+++ src/logger.py
@@ -1,19 +1,26 @@
-import logging 
+import logging
 import os
 from datetime import datetime
 
 LOGS_DIR = "logs"
 os.makedirs(LOGS_DIR, exist_ok=True)
 
-LOG_FILE = os.path.join(LOGS_DIR , f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
-
-logging.basicConfig(
-    filename=LOG_FILE,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    level= logging.INFO
-)
+LOG_FILE = os.path.join(LOGS_DIR, f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
 
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(LOG_FILE)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
     return logger
+
+
